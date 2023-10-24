dbb04eaad44261aff04235d5fe4070f8f24d6baf
Black Rooster <blackrooster2107@gmail.com>
2023-10-24 11:55:50 +0700

    adding next build only

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/ian/Projects/NextJs/animelist/src/app/layout":["static/media/9a881e2ac07d406b-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
